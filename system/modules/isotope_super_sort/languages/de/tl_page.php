58ea2f2d7ff6da1624418ec8f5c40a4ecd2e7c33
kikmedia <ck@kikmedia.de>
2022-10-18 11:30:45 +0200

    German language

---
+++ system/modules/isotope_super_sort/languages/de/tl_page.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Isotope Super Sort
+ *
+ * Copyright (C) 2018-2022 Andrew Stevens Consulting
+ *
+ * @package    asconsulting/isotope_super_sort
+ * @link       https://andrewstevens.consulting
+ */
+
+
+ /**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_page']['iso_product_order'] 			= array("Produktsortierung", "Reihenfolge der Produkte, wie sie auf der Seite erscheinen sollen.");
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_page']['product_sort_legend'] 			= 'Produktsortierung';
